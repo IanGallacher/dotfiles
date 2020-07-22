11c42888616649a6dd09718ccddef481df9d5a14
Ian Gallacher <igallacher@outlook.com>
2020-07-22 10:55:50 -0700

    [py3status] add placehodlder

---
+++ lib/.config/i3/py3status/placeholder.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Used as a placeholder to force the `|` to be drawn before the system tray icons
+"""
+
+
+class Py3status:
+    def placeholder(self):
+        return {
+            'full_text': ' ',
+            'cached_until': self.py3.CACHE_FOREVER
+        }
